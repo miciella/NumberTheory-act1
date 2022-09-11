ad5cf148b4a81ded18dd72c2f3c9402c70b7d28c
miciella <201910804@feualabang.edu.ph>
2022-09-11 09:09:31 +0800

    associative add

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def one():
-    print("Associative (Addition)")
+    # is this right?? lolx
+    print("""\nAssociative (Addition)
+    x+(y+z) = (x+y)+z\n""")
+    x = input("Enter value of x: ")
+    y = input("Enter value of y: ")
+    z = input("Enter value of z: ")
+
+    return f"{x}+({y}+{z}) = ({x}+{y})+{z}"
 
 
 def two():
@@ -55,26 +62,30 @@ def choice_function(number):
     return number_choices.get(number, default)()
 
 
-print("""Properties of Natural Numbers:
-    1. Associative (Addition)
-    2. Commutative (Addition)
-    3. Identity (Addition)
-    4. Associative (Multiplication)
-    5. Commutative (Multiplication)
-    6. Identity (Multiplication)
-    7. Distribution
-    8. Addition is closed
-    9. Multiplication is closed
-    10. Exit""")
+def menu_option():
+    print("""Properties of Natural Numbers:
+        1. Associative (Addition)
+        2. Commutative (Addition)
+        3. Identity (Addition)
+        4. Associative (Multiplication)
+        5. Commutative (Multiplication)
+        6. Identity (Multiplication)
+        7. Distribution
+        8. Addition is closed
+        9. Multiplication is closed
+        10. Exit""")
 
-choice = int(input("Choose a property for Natural Numbers: "))
-print(choice_function(choice))
-try_again = input("Try again? y/n: ").lower()
+    choice = int(input("Choose a property for Natural Numbers: "))
+    print(choice_function(choice))
+
+
+menu_option()
+try_again = input("\nTry again? y/n: ").lower()
+
 
 # lol idk if this is efficient (change if meron pa other way hehe)
 while try_again == 'y':
-    choice = int(input("Choose a property for Natural Numbers: \n"))
-    print(choice_function(choice))
+    menu_option()
     try_again = input("\nTry again? y/n: ").lower()
 
 
