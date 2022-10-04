c24f401b1f261bc52eeda9a8bf21a202159e3a3f
miciella <201910804@feualabang.edu.ph>
2022-10-04 10:35:10 +0800

    removed comments

--- act8-10.py
+++ act8-10.py
@@ -4,24 +4,17 @@ from math import *
 def one():
 
         n = 56
-        # Print the number of two's that divide n
         print("Prime factors of 56:")
         while n % 2 == 0:
             print(2),
             n = n / 2
 
-        # n must be odd at this point
-        # so a skip of 2 ( i = i + 2) can be used
-
         for i in range(3, int(math.sqrt(n)) + 1, 2):
 
-            # while i divides n , print i and divide n
             while n % i == 0:
                 print(i),
                 n = n / i
 
-        # Condition if n is a prime
-        # number greater than 2
         if n > 2:
             return int(n)
 
